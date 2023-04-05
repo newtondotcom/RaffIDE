d06a5a2738bdfa5b5576cfa7b498ff1eb8a1e3eb
thenewservant <ppd491@protonmail.com>
2023-04-05 15:19:29 +0200

    lol

--- src/theAssistantDesRaffinages/Main.java
+++ src/theAssistantDesRaffinages/Main.java
@@ -1,7 +1,24 @@
 package theAssistantDesRaffinages;
 
+import java.awt.BorderLayout;
+import java.awt.Cursor;
+
+import javax.swing.*;
+
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		JFrame frame = new JFrame("Chat");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+        
+        //gère les affichages des raffinages en cours (lecture et modification)
+        ZoneTexte principale = new ZoneTexte();
+        
+        frame.setSize(600, 600);
+        frame.setResizable(false);
+        frame.setVisible(true);
+        frame.setCursor(new Cursor(1));
+        
 	}
 }
