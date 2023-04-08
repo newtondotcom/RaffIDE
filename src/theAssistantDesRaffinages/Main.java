97f2925c6c32e8a3976dae99a931add190f527aa
newtondotcom <102476446+newtondotcom@users.noreply.github.com>
2023-04-08 13:05:13 +0200

    🧃

--- src/theAssistantDesRaffinages/Main.java
+++ src/theAssistantDesRaffinages/Main.java
@@ -1,34 +1,65 @@
 package theAssistantDesRaffinages;
 
-import java.awt.BorderLayout;
-import java.awt.Cursor;
-
 import javax.swing.*;
 import javax.swing.plaf.nimbus.NimbusLookAndFeel;
-import javax.swing.plaf.nimbus.NimbusLookAndFeel;
+import java.awt.*;
 
-public class Main {
-	public static void main(String[] args) throws Exception {
-		// belle interface 
-		
-		UIManager.setLookAndFeel( new NimbusLookAndFeel() );
-		
-		System.out.println("Hello World!");
-		JFrame frame = new JFrame("Chat");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new BorderLayout());
+public class Main extends JFrame {
+
+    public Main() {
+        // Appliquer le look and feel Nimbus
+        try {
+            UIManager.setLookAndFeel(new NimbusLookAndFeel());
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
         
-        //gère les affichages des raffinages en cours (lecture et modification)
-        ZoneTexte principale = new ZoneTexte();
-        	
-        frame.add(principale.getPanel(), BorderLayout.WEST);
-        
-        frame.setSize(600, 600);
-        frame.setResizable(false);
-        frame.setVisible(true);
-        frame.setCursor(new Cursor(1));
-        
+        //Créer le menu
         Menu Menuframe = new Menu();
-        Menuframe.setVisible(true);
-	}
+        
+        //Récupére le menu
+        JMenuBar menuBarreBar = Menuframe.getJMenuBar();
+
+        // Création du conteneur
+        JPanel conteneur = new JPanel();
+
+        // Ajout des zones de texte au conteneur
+        //Zone d'édition du raffinage actif
+        JTextArea edition = new JTextArea();
+        //Permet de pouvoir descendre dans la colonne
+        JScrollPane scrollEdition = new JScrollPane(edition);
+        //Permet de revenir à la ligne quand la fin de la TextArea est atteinte
+        edition.setWrapStyleWord(true);
+        edition.setLineWrap(true);
+        
+        //Zone d'affichage de l'ensemble des raffinages
+        JTextArea affichage = new JTextArea();
+        JScrollPane scrollAffichage = new JScrollPane(affichage);
+        affichage.setWrapStyleWord(true);
+        affichage.setLineWrap(true);
+        
+        // Création du JSplitPane
+        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, scrollEdition, scrollAffichage);
+        //Valeur initiale
+        splitPane.setResizeWeight(0.5);
+
+        // Ajout du conteneur à la fenêtre JFrame
+        add(splitPane);
+        
+        //Ajouter le menu à la fenêtre
+        setJMenuBar(menuBarreBar);
+
+        // Paramètres de la fenêtre
+        setSize(1400, 800);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        
+        
+    }
+
+    public static void main(String[] args) {
+        @SuppressWarnings("unused")
+		Main fenetre = new Main();
+    }
 }
