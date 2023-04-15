3dde698c47f4894f7725668731ed5a74b9488c25
thenewservant <ppd491@protonmail.com>
2023-04-15 18:16:25 +0200

    ajout du menu a droite

--- src/theAssistantDesRaffinages/VueListeRaffinages.java
+++ src/theAssistantDesRaffinages/VueListeRaffinages.java
@@ -2,6 +2,7 @@ package theAssistantDesRaffinages;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
@@ -20,33 +21,31 @@ import javax.swing.SwingUtilities;
 import javax.swing.event.PopupMenuEvent;
 import javax.swing.event.PopupMenuListener;
 
+import javax.swing.tree.TreePath;
+
+
 
 
 public class VueListeRaffinages {
+	private JTree tree;
+	private JPopupMenu popupMenu;
+	private JMenuItem ajouter, supprimer;
+	 public VueListeRaffinages(){
+	      this.tree = new JTree(new Hashtable<>(createTreeData()));
 
-		
-	
-	  public static void main(String[] args) {
-	      JTree tree = new JTree(new Hashtable<>(createTreeData()));
-
-	      JPopupMenu popupMenu = new JPopupMenu();
-	      JMenuItem ajouter = new JMenuItem("Ajouter");
-	      JMenuItem supprimer = new JMenuItem("Supprimer");
+	      popupMenu = new JPopupMenu();
+	      ajouter = new JMenuItem("Ajouter");
+	      supprimer = new JMenuItem("Supprimer");
 	      popupMenu.add(ajouter);
 	      popupMenu.add(supprimer);
-	      ActionListener actionListener = new PopupActionListener();
-	      ajouter.addActionListener(new PopupActionListener());
-	      supprimer.addActionListener(new PopupActionListener());
+	      ActionListener actionListener = new PopupActionListener(tree);
+	      ajouter.addActionListener(actionListener);
+	      supprimer.addActionListener(actionListener);
 	      tree.setComponentPopupMenu(popupMenu);
 
 	      initPopupListener(tree, popupMenu);
 	     
 
-	      JFrame frame = createFrame();
-	      tree.setPreferredSize(new Dimension(200, tree.getPreferredSize().height));
-	      frame.add(new JScrollPane(tree), BorderLayout.WEST);
-	      frame.setLocationRelativeTo(null);
-	      frame.setVisible(true);
 	  }
 
 	  private static void initPopupListener(JTree tree, JPopupMenu popupMenu) {
@@ -78,10 +77,10 @@ public class VueListeRaffinages {
 	  }
 
 	  private static Map<?, ?> createTreeData() {
-	      return Map.of("Sports",
-	              new String[]{"Mustang", "Corvette", "Ferrari"},
-	              "Luxury",
-	              new String[]{"BMW", "Mercedes-Benz", "Rolls-Royce"});
+	      return Map.of("R1: Comment machin?",
+	              new String[]{"R2: Faire truc1", "R2 :Manger des pates"},
+	              "R1: Comment truc?",
+	              new String[]{"BMW", "R2: Faire truc3", "Rolls-Royce"});
 	  }
 
 	  private static JFrame createFrame() {
@@ -92,13 +91,21 @@ public class VueListeRaffinages {
 	  }
 
 	public JScrollPane getScrollPane() {
-		// TODO Auto-generated method stub
-		return null;
+		return new JScrollPane(this.tree);
 	}
 	}
 
  class PopupActionListener implements ActionListener {
+	 private JTree arbre;
+	 
+		public PopupActionListener(JTree arb) {
+			this.arbre = arb;
+		}
 	  public void actionPerformed(ActionEvent actionEvent) {
-	    System.out.println("trace: " + actionEvent.getActionCommand());
+		  final Font currentFont = arbre.getFont();
+		  final Font bigFont = new Font(currentFont.getName(), currentFont.getStyle(), currentFont.getSize() + 2);
+		  //arbre.setFont(bigFont);
+		  TreePath path = arbre.getSelectionPath();
+	    System.out.println("trace: " + path.getLastPathComponent().getClass());
 	  }
 	}
