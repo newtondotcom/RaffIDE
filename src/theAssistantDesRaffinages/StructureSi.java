c5abac85a35ba77bb9aa7ef041fb34baab69ec43
Amandine-Gravier <gravieramandine02@gmail.com>
2023-05-10 11:05:24 +0200

    modifications sur structures de controle

--- src/theAssistantDesRaffinages/StructureSi.java
+++ src/theAssistantDesRaffinages/StructureSi.java
@@ -12,8 +12,8 @@ public class StructureSi extends StructureDeControle {
 	public String toString() {
 		String stringToAppend = "Si (" + this.getCondition() + ") Alors :" ;
 		for (Element struct : this.getCorps()) {
-			stringToAppend += "\t";
-			// TODO Gerer le Corps
+			stringToAppend += "\n\t";
+			stringToAppend += struct.toString();
 		}
 		stringToAppend += "\nFinSi\n";
 		return stringToAppend;		
--- src/theAssistantDesRaffinages/StructureSiSinon.java
+++ src/theAssistantDesRaffinages/StructureSiSinon.java
@@ -1,22 +1,25 @@
 package theAssistantDesRaffinages;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public abstract class StructureSiSinon extends StructureDeControle {
 
+	List<Element> SinonCorps;
     public StructureSiSinon(String condition, String nom) {
         super(condition, nom);
+        SinonCorps = new ArrayList<>();
     }
-
+    
     @Override
     public String toString() {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append(TextFormat.BOLD.getCode()); // Appliquer le format en gras
         stringBuilder.append(TextColor.YELLOW.getCode()); // Appliquer la couleur jaune
         stringBuilder.append("Si (").append(getCondition()).append(") Alors :");
-        stringBuilder.append(TextFormat.RESET.getCode()); // Réinitialiser le format
+        stringBuilder.append(TextFormat.RESET.getCode()); // Rï¿½initialiser le format
 
-        // Affichage des éléments dans la partie "Alors"
+        // Affichage des Ã©lÃ©ments dans la partie "Alors"
         for (Element struct : getCorps()) {
             stringBuilder.append("\n\t").append(struct.toString());
         }
@@ -24,17 +27,17 @@ public abstract class StructureSiSinon extends StructureDeControle {
         stringBuilder.append(TextFormat.BOLD.getCode()); // Appliquer le format en gras
         stringBuilder.append(TextColor.BLUE.getCode()); // Appliquer la couleur bleue
         stringBuilder.append("\nSinon :");
-        stringBuilder.append(TextFormat.RESET.getCode()); // Réinitialiser le format
+        stringBuilder.append(TextFormat.RESET.getCode()); // RÃ©initialiser le format
 
-        // Affichage des éléments dans la partie "Sinon"
-        for (Element struct : getSinonCorps()) {
+        // Affichage des Ã©lÃ©ments dans la partie "Sinon"
+        for (Element struct : SinonCorps) {
             stringBuilder.append("\n\t").append(struct.toString());
         }
 
         stringBuilder.append(TextFormat.BOLD.getCode()); // Appliquer le format en gras
         stringBuilder.append(TextColor.YELLOW.getCode()); // Appliquer la couleur jaune
         stringBuilder.append("\nFinSi\n");
-        stringBuilder.append(TextFormat.RESET.getCode()); // Réinitialiser le format
+        stringBuilder.append(TextFormat.RESET.getCode()); // Rï¿½initialiser le format
 
         return stringBuilder.toString();
     }
