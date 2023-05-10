d9e09c59a71832b60177117a8eeba31f12135827
antonin-tarrade <antotarrade@gmail.com>
2023-05-10 15:03:42 +0200

    Fix handle Sdc

--- src/theAssistantDesRaffinages/StructureDeControleListener.java
+++ src/theAssistantDesRaffinages/StructureDeControleListener.java
@@ -8,21 +8,24 @@ import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
 
 public class StructureDeControleListener implements ActionListener {
-	
+
 	/** La structure de controle a observer */
 	private Class<? extends StructureDeControle> classe;
-	
+
 	/** La zone d'edition */
 	private VueEditionRaffinages vueEd;
 
 	/** Creer un observateur de structure de controle */
-	public StructureDeControleListener(Class<? extends StructureDeControle> class1,VueEditionRaffinages vueEd) {
+	public StructureDeControleListener(
+			Class<? extends StructureDeControle> class1,
+			VueEditionRaffinages vueEd) {
 		this.classe = class1;
 		this.vueEd = vueEd;
 	}
-	
+
 	/**
 	 * Action se declanchant lorsque l'on appuie sur le bouton
+	 * 
 	 * @param e L'evenement (appuyer sur le bouton)
 	 */
 	@Override
@@ -30,12 +33,18 @@ public class StructureDeControleListener implements ActionListener {
 		StructureDeControle sdc;
 		if (classe == StructurePour.class) {
 			/* À changer */
-			String var = JOptionPane.showInputDialog("Entrez un nom de variable");
-			String Debut = JOptionPane.showInputDialog("Entrez le début de la boucle");
-			String Fin = JOptionPane.showInputDialog("Entrez la fin de la boucle");
+			String var = JOptionPane
+					.showInputDialog("Entrez un nom de variable");
+			String Debut = JOptionPane
+					.showInputDialog("Entrez le début de la boucle");
+			String Fin = JOptionPane
+					.showInputDialog("Entrez la fin de la boucle");
 			System.out.println(e.getSource().getClass());
 			try {
-				sdc = classe.getConstructor(new Class<?>[]{String.class,String.class,String.class,String.class}).newInstance("",var,Debut,Fin);
+				sdc = classe
+						.getConstructor(new Class<?>[] { String.class,
+								String.class, String.class, String.class })
+						.newInstance("", var, Debut, Fin);
 				vueEd.getRaffCourant().addElement(sdc);
 			} catch (InstantiationException | IllegalAccessException
 					| IllegalArgumentException | InvocationTargetException
@@ -44,11 +53,15 @@ public class StructureDeControleListener implements ActionListener {
 				e1.printStackTrace();
 			}
 		}
-		
+
 		else {
-			String condition = JOptionPane.showInputDialog("Entrez une condition");
+			String condition = JOptionPane
+					.showInputDialog("Entrez une condition");
 			try {
-				sdc = classe.getConstructor(new Class<?>[]{String.class,String.class}).newInstance(condition,"");
+				sdc = classe
+						.getConstructor(
+								new Class<?>[] { String.class, String.class })
+						.newInstance(condition, "");
 				vueEd.getRaffCourant().addElement(sdc);
 			} catch (InstantiationException | IllegalAccessException
 					| IllegalArgumentException | InvocationTargetException
@@ -57,8 +70,7 @@ public class StructureDeControleListener implements ActionListener {
 				e1.printStackTrace();
 			}
 		}
-		
-		
+
 		vueEd.update();
 	}
 
